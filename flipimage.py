7c681ab051e2ab51a576bb2aa91598db1f580208
vasubhuva <avasu4475@gmail.com>
2023-01-28 09:35:50 +0530

    Signed-off-by: vasubhuva <avasu4475@gmail.com>

--- flipimage.py
+++ flipimage.py
@@ -1,19 +1,22 @@
 import cv2
 
 img1=cv2.imread("B:\\sem4\\Image-Processing\\Images\\1296932.png")
-img1=cv2.resize(img1,(1000,700))
-img1=cv2.flip(img1,0) # we can pass 0,1,-1 as second parameter
+img1=cv2.resize(img1,(1000,700))\
+    
+# we can pass 0,1,-1 as second parameter it will change the direction of image
+img1=cv2.flip(img1,0) 
 # img1=cv2.flip(img1,1)
 # img1=cv2.flip(img1,-1)
 
+img1=cv2.cvtColor(img1,cv2.COLOR_BGR2HSV)
 cv2.imshow("image",img1)
 # Just for checking purpose
 # cv2.waitKey(0)
 # cv2.destroyAllWindows()
 
-# k=cv2.waitKey(0)
+k=cv2.waitKey(0)
 
-# if k==ord("s"):
-#     cv2.imwrite("B:\\sem4\\Image-Processing\\OutputImages\\flip.png",img1)
-# else:
-#     cv2.destroyAllWindows()
+if k==ord("s"):
+    cv2.imwrite("B:\\sem4\\Image-Processing\\OutputImages\\flip.png",img1)
+else:
+    cv2.destroyAllWindows()
---
+++ imagecomparison.py
@@ -0,0 +1,22 @@
+import cv2
+from PIL import Image
+import imagehash
+
+img1=imagehash.average_hash(Image.open("Image-Processing\\OutputImages\\flip3.png"))
+img2=imagehash.average_hash(Image.open("Image-Processing\\OutputImages\\flip4.png"))
+diff=img1-img2
+# print(diff)
+
+# original image and gray converted image will be considered as same 
+# converted to other colors will be considered as different
+if img1==img2:
+    print("same")
+else:
+    print("not")
+    print(img1-img2)
+
+if diff==0:
+    print("same")
+else:
+    print("not")
+    print(diff)
